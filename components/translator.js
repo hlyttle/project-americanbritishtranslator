1beaa89b5289c2326a651f6823483421e228bd27
Helen Lyttle <helenlyttle@live.co.uk>
2024-09-13 12:27:41 +0100

    Mapped out desired logic in translator.js

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,17 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
-class Translator {
+class Translator {}
 
-}
+//take a string and an desired language
+//split the string by spaces
+//check for each word (lower case) in relevant dictionary.keys
+//if not there, continue
+//if present, replace with relevant word, capitalising first letter if necessary
+//repeat with titles (keys or actual word as relevant)
+//repeat with spelling (keys or actual word as relevant)
+//return original string + new string (api can handle something with no changes)
 
 module.exports = Translator;
