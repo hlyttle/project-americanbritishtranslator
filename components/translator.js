fc1dd835e5f1a21446171761f8616d679947696a
Helen Lyttle <helenlyttle@live.co.uk>
2024-09-13 16:25:51 +0100

    Built all unit tests for translator function

--- components/translator.js
+++ components/translator.js
@@ -6,12 +6,16 @@ const britishOnly = require("./british-only.js");
 class Translator {}
 
 //take a string and an desired language
-//split the string by spaces
+//split the string by spaces ** THIS WON'T WORK as some are phrases, not words
+//lower case full sentence and do a sentence.includes? but what about parts of words?
+//do i need to check if before it is anything other than a space or nothing, and after it is anything other than a space or punctuation or nothing?
 //check for each word (lower case) in relevant dictionary.keys
 //if not there, continue
 //if present, replace with relevant word, capitalising first letter if necessary
 //repeat with titles (keys or actual word as relevant)
 //repeat with spelling (keys or actual word as relevant)
-//return original string + new string (api can handle something with no changes)
+//a : between two numbers is turned to a ., or vice versa, and the whole number is wrapped
+//puts highlight span around any changed words
+//return translated string (api can handle something with no changes)
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,32 +2,199 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite("Unit Tests", () => {});
-
-/*
-Translate Mangoes are my favorite fruit. to British English
-Translate I ate yogurt for breakfast. to British English
-Translate We had a party at my friend's condo. to British English
-Translate Can you toss this in the trashcan for me? to British English
-Translate The parking lot was full. to British English
-Translate Like a high tech Rube Goldberg machine. to British English
-Translate To play hooky means to skip class or work. to British English
-Translate No Mr. Bond, I expect you to die. to British English
-Translate Dr. Grosh will see you now. to British English
-Translate Lunch is at 12:15 today. to British English
-Translate We watched the footie match for a while. to American English
-Translate Paracetamol takes up to an hour to work. to American English
-Translate First, caramelise the onions. to American English
-Translate I spent the bank holiday at the funfair. to American English
-Translate I had a bicky then went to the chippy. to American English
-Translate I've just got bits and bobs in my bum bag. to American English
-Translate The car boot sale at Boxted Airfield was called off. to American English
-Translate Have you met Mrs Kalyani? to American English
-Translate Prof Joyner of King's College, London. to American English
-Translate Tea time is usually around 4 or 4.30. to American English
-Highlight translation in Mangoes are my favorite fruit.
-Highlight translation in I ate yogurt for breakfast.
-Highlight translation in We watched the footie match for a while.
-Highlight translation in Paracetamol takes up to an hour to work.
-*/
+suite("Unit Tests", () => {
+  suite("Translating to British English", () => {
+    test("favorite", () => {
+      assert.equal(
+        translator.translate("Mangoes are my favorite fruit.", "British"),
+        "Mangoes are my <span class='highlight'>favourite</span> fruit."
+      );
+    });
+    test("yogurt", () => {
+      assert.equal(
+        translator.translate("I ate yogurt for breakfast.", "British"),
+        "I ate <span class='highlight'>yoghurt</span> for breakfast."
+      );
+    });
+    test("condo", () => {
+      assert.equal(
+        translator.translate("We had a party at my friend's condo.", "British"),
+        "We had a party at my friend's <span class='highlight'>flat</span>."
+      );
+    });
+    test("trashcan", () => {
+      assert.equal(
+        translator.translate(
+          "Can you toss this in the trashcan for me?",
+          "British"
+        ),
+        "Can you toss this in the <span class='highlight'>bin</span> for me?"
+      );
+    });
+    test("parking lot", () => {
+      assert.equal(
+        translator.translate("The parking lot was full.", "British"),
+        "The <span class='highlight'>car park</span> was full."
+      );
+    });
+    test("Rube Goldberg machine", () => {
+      assert.equal(
+        translator.translate(
+          "Like a high tech Rube Goldberg machine.",
+          "British"
+        ),
+        "Like a high tech <span class='highlight'>Heath Robinson device</span>."
+      );
+    });
+    test("play hooky", () => {
+      assert.equal(
+        translator.translate(
+          "To play hooky means to skip class or work.",
+          "British"
+        ),
+        "To <span class='highlight'>bunk off</span> means to skip class or work."
+      );
+    });
+    test("Mr.", () => {
+      assert.equal(
+        translator.translate("No Mr. Bond, I expect you to die.", "British"),
+        "No <span class='highlight'>Mr</span> Bond, I expect you to die."
+      );
+    });
+    test("Dr.", () => {
+      assert.equal(
+        translator.translate("Dr. Grosh will see you now.", "British"),
+        "<span class='highlight'>Dr</span> Grosh will see you now."
+      );
+    });
+    test("digital times", () => {
+      assert.equal(
+        translator.translate("Lunch is at 12:15 today.", "British"),
+        "Lunch is at <span class='highlight'>12.15</span> today."
+      );
+    });
+  });
+  suite("Translating to American English", () => {
+    test("footie", () => {
+      assert.equal(
+        translator.translate(
+          "We watched the footie match for a while.",
+          "American"
+        ),
+        "We watched the <span class='highlight'>soccer</span> match for a while."
+      );
+    });
+    test("Paracetamol", () => {
+      assert.equal(
+        translator.translate(
+          "Paracetamol takes up to an hour to work.",
+          "American"
+        ),
+        "<span class='highlight'>Tylenol</span> takes up to an hour to work."
+      );
+    });
+    test("caramelise", () => {
+      assert.equal(
+        translator.translate("First, caramelise the onions.", "American"),
+        "First, <span class='highlight'>caramelize</span> the onions."
+      );
+    });
+    test("bank holiday, funfair", () => {
+      assert.equal(
+        translator.translate(
+          "I spent the bank holiday at the funfair.",
+          "American"
+        ),
+        "I spent the <span class='highlight'>public holiday</span> at the <span class='highlight'>carnival</span>."
+      );
+    });
+    test("bicky, chippy", () => {
+      assert.equal(
+        translator.translate(
+          "I had a bicky then went to the chippy.",
+          "American"
+        ),
+        "I had a <span class='highlight'>cookie</span> then went to the <span class='highlight'>fish-and-chip shop</span>."
+      );
+    });
+    test("bits and bobs, bum bag", () => {
+      assert.equal(
+        translator.translate(
+          "I've just got bits and bobs in my bum bag.",
+          "American"
+        ),
+        "I've just got <span class='highlight'>odds and ends</span> in my <span class='highlight'>fanny pack</span>."
+      );
+    });
+    test("car boot sale", () => {
+      assert.equal(
+        translator.translate(
+          "The car boot sale at Boxted Airfield was called off.",
+          "American"
+        ),
+        "The <span class='highlight'>swap meet</span> at Boxted Airfield was called off."
+      );
+    });
+    test("Mrs", () => {
+      assert.equal(
+        translator.translate("Have you met Mrs Kalyani?", "American"),
+        "Have you met <span class='highlight'>Mrs.</span> Kalyani?"
+      );
+    });
+    test("Prof", () => {
+      assert.equal(
+        translator.translate(
+          "Prof Joyner of King's College, London.",
+          "American"
+        ),
+        "<span class='highlight'>Prof.</span> Joyner of King's College, London."
+      );
+    });
+    test("digital times", () => {
+      assert.equal(
+        translator.translate(
+          "Tea time is usually around 4 or 4.30.",
+          "American"
+        ),
+        "Tea time is usually around 4 or <span class='highlight'>4:30</span>."
+      );
+    });
+  });
+  suite("Verifying highlights", () => {
+    /*
+    NB. This additional section is completely redundant as it replicates four of the tests above; however, freeCodeCamp requires these tests in order to complete their challenge, and the only other approach would be to create a separate 'add/remove highlight' function which would only be useful in the specific case of these tests.
+    */
+    test("favorite", () => {
+      assert.equal(
+        translator.translate("Mangoes are my favorite fruit.", "British"),
+        "Mangoes are my <span class='highlight'>favourite</span> fruit."
+      );
+    });
+    test("yogurt", () => {
+      assert.equal(
+        translator.translate("I ate yogurt for breakfast.", "British"),
+        "I ate <span class='highlight'>yoghurt</span> for breakfast."
+      );
+    });
+    test("footie", () => {
+      assert.equal(
+        translator.translate(
+          "We watched the footie match for a while.",
+          "American"
+        ),
+        "We watched the <span class='highlight'>soccer</span> match for a while."
+      );
+    });
+    test("Paracetamol", () => {
+      assert.equal(
+        translator.translate(
+          "Paracetamol takes up to an hour to work.",
+          "American"
+        ),
+        "<span class='highlight'>Tylenol</span> takes up to an hour to work."
+      );
+    });
+  });
+});
